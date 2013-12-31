4610f1a2ac9b521c082c00e3a789c12308998f37
Paul Oliver <bitbucket@paultastic.com>
2013-12-31 11:04:31 -0600

    Converting to a bit of nunit/FakeItEasy to allow for easier testing
    In order to feel better about RoundhousE I'd like to test the migration runners.
    I wasn't able to do this easy using bdd. I'm creating a project for using NUnit
    and FakeItEasy to allow me to more easily test the internals and to allow for
    a bit higher code coverage around crucial functionality.

---
+++ roundhouse.unittests/CryptographicServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Security.Cryptography;
+using NUnit.Framework;
+using roundhouse.cryptography;
+
+namespace roundhouse.unittests
+{
+    [TestFixture]
+    public class CryptographicServiceTests
+    {
+        [Test]
+        public void CreateMD5Cipher_WithString_GivesMeTheCorrectNumberOfBytes()
+        {
+            var md5crypto           = MD5.Create();
+            var text_to_hash        = "I want to see what the freak is going on here";
+            byte[] clear_text_bytes = Encoding.UTF8.GetBytes(text_to_hash);
+            byte[] cypher_bytes     = md5crypto.ComputeHash(clear_text_bytes);
+            Assert.AreEqual(16, cypher_bytes.Length);
+            Debug.WriteLine(cypher_bytes);
+            string base_64_cypher   = Convert.ToBase64String(cypher_bytes);
+            Assert.AreEqual(24, base_64_cypher.Length);
+            Debug.WriteLine(base_64_cypher);
+        }
+
+        [Test]
+        public void Hash_WithString_ReturnsProperString()
+        {
+
+            var md5_crypto       = new MD5CryptographicService();
+            string text_to_hash  = "I want to see what the freak is going on here";
+            string expected_hash = "TMGPZJmBhSO5uYbf/TBqNA==";
+            Assert.AreEqual(expected_hash, md5_crypto.hash(text_to_hash));
+        }
+    }
+}
